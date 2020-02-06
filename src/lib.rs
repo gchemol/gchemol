0817751350bb6660e06af9bbdc0b5d31bf51cf8b
Wenping Guo <ybyygu@gmail.com>
2020-02-06 17:13:28 +0800

    Exports gchemol_geometry crate as geom

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //        AUTHOR:  Wenping Guo <ybyygu@gmail.com>
 //       LICENCE:  GPL version 2 or upper
 //       CREATED:  <2018-04-10 Tue 15:46>
-//       UPDATED:  <2020-02-05 Wed 08:51>
+//       UPDATED:  <2020-02-06 Thu 17:11>
 //===============================================================================#
 // header:1 ends here
 
@@ -18,11 +18,10 @@
 
 // [[file:~/Workspace/Programming/gchemol-rs/gchemol-new/gchemol-new.note::*exports][exports:1]]
 pub use gchemol_core::*;
+pub use gchemol_geometry as geom;
 pub use gchemol_readwrite as io;
 
 pub mod prelude {
     pub use gchemol_readwrite::prelude::*;
-    // pub use gchemol_geometry::*;
-    // pub use gchemol_geometry::prelude::*;
 }
 // exports:1 ends here
