9fa951350de9ad614be823de0c40b0a20835d6e4
Wenping Guo <ybyygu@gmail.com>
2020-02-21 14:09:54 +0800

    update test for `render_with` method

--- tests/io.rs
+++ tests/io.rs
@@ -44,6 +44,7 @@ fn read_write_molecules() {
     for mol in gchemol::io::read(&f).expect("read mol2") {
         //
     }
+
     // read all molecules into a Vec
     let mols = gchemol::io::read_all(&f).expect("read all mol2");
     assert_eq!(mols.len(), 6);
@@ -71,6 +72,6 @@ fn read_write_molecules() {
 
     // format molecule using user defined template
     let tpl = "./tests/files/templates/xyz.tera";
-    let s = mol.render_with(tpl).expect("render tera");
+    let s = mol.render_with(tpl.as_ref()).expect("render tera");
     assert!(!s.is_empty());
 }
