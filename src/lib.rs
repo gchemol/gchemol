417a1549c678305c7dc15fd075121fd507358b24
Wenping Guo <ybyygu@gmail.com>
2022-03-26 10:18:05 +0800

    New unit fs

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //        AUTHOR:  Wenping Guo <ybyygu@gmail.com>
 //       LICENCE:  GPL version 2 or upper
 //       CREATED:  <2018-04-10 Tue 15:46>
-//       UPDATED:  <2022-01-02 Sun 16:07>
+//       UPDATED:  <2022-03-26 Sat 10:17>
 //===============================================================================#
 // header:1 ends here
 
@@ -26,6 +26,7 @@ pub mod geom {
 }
 
 pub mod prelude {
+    pub use gchemol_geometry::prelude::*;
     pub use gchemol_readwrite::prelude::*;
 }
 
@@ -33,11 +34,18 @@ pub mod prelude {
 #[allow(non_upper_case_globals)]
 pub mod units {
     pub const eV: f64 = 1.0;
+
     pub const Angstrom: f64 = 1.0;
+
     pub const Hartree: f64 = 27.211386024367243;
+
     pub const Bohr: f64 = 0.5291772105638411;
+
     /// Boltzmann constant
     pub const kB: f64 = 8.617330337217213E-05;
+
+    /// femtosecond
+    pub const fs: f64 = 0.09822694788464063;
 }
 // 66712791 ends here
 
