3d9931ac6e7eeb64f93352e5a97aa6504f3980e7
Wenping Guo <ybyygu@gmail.com>
2023-01-27 13:09:11 +0800

    Add test for superimpose_onto

--- tests/geometry.rs
+++ tests/geometry.rs
@@ -5,7 +5,7 @@ use gchemol::prelude::*;
 use gchemol::Molecule;
 use vecfx::approx::assert_relative_eq;
 
-// mol
+// measurement
 // #+name: 91197950
 
 #[test]
@@ -14,4 +14,19 @@ fn test_mol_measure_geom() {
     assert_relative_eq!(1.35520, mol.get_distance(1, 4).unwrap(), epsilon = 1E-3);
     assert_relative_eq!(119.887, mol.get_angle(3, 1, 4).unwrap().to_degrees(), epsilon = 1E-3);
     assert_relative_eq!(0.0, mol.get_torsion(2, 1, 4, 6).unwrap().to_degrees(), epsilon = 1E-3);
+}
+
+// superimpose
+// #+name: 049603c7
+
+#[test]
+fn test_superimpose() {
+    let mut mol = Molecule::from_database("CH4");
+    let mol_ref = mol.clone();
+    mol.translate([0.5, 1.0, 2.0]);
+    let rmsd = mol.superimpose_onto(&mol_ref, None);
+    assert_relative_eq!(rmsd, 0.0, epsilon = 1E-5);
+
+    let rmsd = mol.superimpose_onto(&mol_ref, Some(&[1, 2, 3, 4]));
+    assert_relative_eq!(rmsd, 0.0, epsilon = 1E-5);
 }
